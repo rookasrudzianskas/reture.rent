21d866b0c54cba516d8054dd0a6a88162ba9a5f2
Rokas <rookas.rudzenskas@gmail.com>
2021-07-03 08:51:07 +0300

    Rokas

---
+++ src/components/BodyImage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles/BodyImage.css";
+
+const BodyImage = () => {
+    return (
+        <div className="bodyImage">
+            <div className="bodyImage__image">
+                <img src="/images/home.png" alt=""/>
+            </div>
+        </div>
+    );
+};
+
+export default BodyImage;
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../../components/Header";
 import HomeShit from "../../components/HomeShit";
+import BodyImage from "../../components/BodyImage";
 
 const HomeScreen = () => {
     return (
         <div className="homescreen">
             <Header />
             <HomeShit />
+            <BodyImage />
         </div>
     );
 };
