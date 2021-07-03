dc549ba6a8258190fbdd8d389136639311d7340e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-03 10:15:02 +0300

    Rokas

---
+++ src/components/RentureApp.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./styles/PMS.css";
+
+
+const PMS = () => {
+    return (
+        <div className="PMS">
+            <div className="PMS__margin">
+            <div className="PMS__leftRight__section">
+                <div className="PMS__image">
+                    <div className="PMS__image__real">
+                        <img src="/images/how-it-works-illustration.png" alt=""/>
+                    </div>
+                </div>
+                <div className="PMS__text">
+                    <p className="PMS__howItWorks">HOW IT WORKS</p>
+                    <div className="smaller__width">
+                        <h1 className="PMS__howToUnify">Unify your PMS and service provider partners</h1>
+                    </div>
+
+                    <div className="smaller__width">
+                        <p className="PMS__description">Unify your PMS and service provider partners within a seamless tenant engagement platform, and wave goodbye to paper and email workflows.</p>
+                    </div>
+                    <h3>Join now to start earning Rewards.*</h3>
+
+                    <div className="cta">
+                        Join now
+                    </div>
+                </div>
+
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default PMS;
