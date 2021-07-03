e617c8b1c4eeb56ea101f5bda4d877621a3fda6a
Rokas <rookas.rudzenskas@gmail.com>
2021-07-03 14:10:54 +0300

    Roaks

---
+++ src/components/BannerClick.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+const BannerClick = () => {
+
+
+    function getModalStyle() {
+        const top = 50;
+        const left = 50;
+
+        return {
+            top: `${top}%`,
+            left: `${left}%`,
+            transform: `translate(-${top}%, -${left}%)`,
+        };
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }));
+
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    return (
+        <div className="bannerClick">
+            <div>
+                <button type="button" onClick={handleOpen}>
+                    Open Modal
+                </button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                >
+                </Modal>
+            </div>
+        </div>
+    );
+};
+
+export default BannerClick;
