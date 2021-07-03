9b73f49aa5fae4fd95080b8107c8ce264f23ded6
Rokas <rookas.rudzenskas@gmail.com>
2021-07-03 15:37:45 +0300

    Rokas

--- src/screens/SearchResultScreen/SearchResultScreen.js
+++ src/screens/SearchResultScreen/SearchResultScreen.js
@@ -3,10 +3,12 @@ import SearchResult from "../../components/SearchResult";
 import SearchPage from "../../components/SearchPage";
 import "../../../src/components/styles/SearchResultScreen.css";
 import Footer from "../../components/Footer";
+import HeaderDashboard from "../../components/Header-Dashboard";
 
 const SearchResultScreen = () => {
     return (
         <div className="backgroundColor">
+            <HeaderDashboard />
             <SearchPage />
             <SearchResult />
             <Footer />
