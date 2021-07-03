1237b0a92cc998c79d2a2ed869026f0609b9d42e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-03 06:54:48 +0300

    Rokas:

---
+++ src/features/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export const selectCount = (state) => state.counter.value;
+
+
+export default counterSlice.reducer;
---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <div className="homescreen">
+
+        </div>
+    );
+};
+
+export default HomeScreen;
