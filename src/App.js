fbac46c15d3664673a8afc7eaacc47e3b03e1493
Rokas <rookas.rudzenskas@gmail.com>
2021-07-07 15:07:41 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -82,7 +82,6 @@ function App() {
                             <HomeScreen />
                             <HomeScreen />
                             <HomeScreen />
-                            <HomeScreen />
                         </Route>
 
                     </Switch>
