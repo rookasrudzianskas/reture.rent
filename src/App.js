b48189ca96feab9bd3f935e3c1bda6ac33d28083
Rokas <rookas.rudzenskas@gmail.com>
2021-07-09 20:34:20 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -83,7 +83,6 @@ function App() {
                         </Route>
                         </Route>
                         </Route>
-                        </Route>
 
                     </Switch>
 
