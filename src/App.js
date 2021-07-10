95e3e59692555f0fcb705eb49d10e1af16e86ae5
Rokas <rookas.rudzenskas@gmail.com>
2021-07-10 14:48:39 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -103,11 +103,4 @@ function App() {
 }
 
 export default App;
-export default App;
-export default App;
-export default App;
-export default App;
-export default App;
-export default App;
-export default App;
-export default App;
+
