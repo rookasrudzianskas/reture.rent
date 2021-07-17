6a66d0a50c940f1f87d7073900359592f70ee186
Rokas <rookas.rudzenskas@gmail.com>
2021-07-17 18:05:41 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -90,7 +90,6 @@ function App() {
                                             <Dashboard/>
                                             <Dashboard/>
                                             <Dashboard/>
-                                            <Dashboard/>
                                     </Route>
                                     )
                                 }
